6aa5d9275f9f055a6e2b47fd248ed1d939d33c69
38752 <118719220+38752@users.noreply.github.com>
2023-02-11 17:24:50 +0900

    ビューの追加、編集

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\TasksController;
+use App\Http\Controllers\TasksController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
