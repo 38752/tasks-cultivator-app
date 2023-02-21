3cfd0548a76db20ffff749604f0ca537502e45e5
38752 <118719220+38752@users.noreply.github.com>
2023-02-21 11:15:58 +0900

    BASIC認証の導入

--- routes/web.php
+++ routes/web.php
@@ -15,30 +15,33 @@ use App\Http\Controllers\TasksController;
 |
 */
 
-// ログインしている場合のルーティング
-Route::group(['middleware' => ['auth']], function() {
-    Route::get('/', function () {
-        return redirect('/calendar');
+// BASIC認証
+Route::group(['middleware' => 'basicauth'], function() {
+    // ログインしている場合のルーティング
+    Route::group(['middleware' => ['auth']], function() {
+        Route::get('/', function () {
+            return redirect('/calendar');
+        });
+
+        Route::get('/dashboard', function () {
+            return redirect('/calendar');
+        })->middleware(['auth', 'verified'])->name('dashboard');
+
+        Route::middleware('auth')->group(function () {
+            Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+            Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+            Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        });
+
+        Route::get('/calendar', function () {
+            return view('calendar');
+        });
+
+        Route::resource('/tasks', TasksController::class, ['only' => ['index', 'store', 'update', 'destroy']]);
+        Route::get('/tasks/search', [TasksController::class, 'searchTasks'])->name('search-tasks');
+
+        Route::post('/tasks-get', [TasksController::class, 'getTasks'])->name('tasks-get');
     });
 
-    Route::get('/dashboard', function () {
-        return redirect('/calendar');
-    })->middleware(['auth', 'verified'])->name('dashboard');
-
-    Route::middleware('auth')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    });
-
-    Route::get('/calendar', function () {
-        return view('calendar');
-    });
-
-    Route::resource('/tasks', TasksController::class, ['only' => ['index', 'store', 'update', 'destroy']]);
-    Route::get('/tasks/search', [TasksController::class, 'searchTasks'])->name('search-tasks');
-
-    Route::post('/tasks-get', [TasksController::class, 'getTasks'])->name('tasks-get');
+    require __DIR__.'/auth.php';
 });
-
-require __DIR__.'/auth.php';
