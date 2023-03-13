f3f8aced09e070c34c6a27c64eaf7eacde7b7456
38752 <118719220+38752@users.noreply.github.com>
2023-03-13 20:06:06 +0900

    JavaScriptの不要なコードを削除

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,5 @@
 import './bootstrap';
 import './calendar';
-import './show_only_one_task';
-import './store_task';
-import './edit_task';
-import './update_task';
-import './delete_task';
 import './search_task';
 import './select_task';
 import './hide_navbar_search';
